e2c0a1b2da741bcf47628c66372e1b8dd0db7b47
Esti Shay and Kate Byars <esti@>
2017-07-05 11:44:16 -0700

    add remove function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
   $("button#unicorn").click(function() {
     $("ul#user").prepend("<li>Unicorn</li>");
     $("ul#webpage").prepend("<li>Dragon</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+    });
   });
 
   $("button#dragon").click(function() {
     $("ul#user").prepend("<li>Dragon</li>");
     $("ul#webpage").prepend("<li>Unicorn</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+    });
   });
 });
